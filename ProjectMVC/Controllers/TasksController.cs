549a0cefb62e7f7a05625fcff53e03da600d0307
David Santafe Zorrilla <santafedavid01@gmail.com>
2019-04-11 17:28:03 -0500

    Se agrega vista de calendario para las tareas con pluggin de fullcalendar

--- ProjectMVC/Controllers/TasksController.cs
+++ ProjectMVC/Controllers/TasksController.cs
@@ -87,5 +87,95 @@ namespace ProjectMVC.Controllers
 
             return View(model);
         }
+
+        private readonly Dictionary<int, string> ColorState = new Dictionary<int, string> {
+            { 1, "#FFFF00" },
+            { 2, "#FF8000" },
+            { 3, "#088A08" }
+        };
+
+        public ActionResult Calendar(int? projectId)
+        {
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetProjects(projectId, null).FirstOrDefault();
+
+            ViewBag.Project = project;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalendarJSON(int? projectId)
+        {
+            try
+            {
+                Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+                var listTasks = tasks.GetTasks(projectId, null);                
+
+                var listTasksCalendarViewModel = listTasks.Select(x => new Logica.Models.ViewModels.TasksCalendarViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    AllDay = true,
+                    Color = ColorState.FirstOrDefault(y => y.Key == x.StateId).Value,
+                    Start = x.ExpirationDate.Value.AddDays(Convert.ToDouble(-x.RemainingWork)).ToString("yyyy-MM-dd HH:mm:ss"),
+                    End = x.ExpirationDate.Value.ToString("yyyy-MM-dd HH:mm:ss"),
+                    TextColor = "#000000"
+                }).ToList();
+                
+                var json = Json(new
+                {
+                    ListTasksCalendar = listTasksCalendarViewModel,
+                    IsSuccessful = true
+                });
+
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return Json(new Models.ResponseViewModel
+                {
+                    IsSuccessful = false,
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        public ActionResult GetTasksJSON(int? id)
+        {
+            try
+            {
+                Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+                var listTasks = tasks.GetTasks(null, id);
+
+                var taskViewModel = listTasks.Select(x => new Logica.Models.ViewModels.TasksDetailsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Details = x.Details,
+                    ExpirationDate = x.ExpirationDate.Value.ToString("yyyy-MM-dd HH:mm:ss"),
+                    Effort = x.Effort,
+                    RemainingWork = x.RemainingWork,
+                    State = x.States.Name,
+                    Activity = x.Activities.Name,
+                    Priority = x.Priorities.Name
+                }).FirstOrDefault();
+
+                var json = Json(new
+                {
+                    Task = taskViewModel,
+                    IsSuccessful = true
+                });
+
+                return json;                
+            }
+            catch (Exception ex)
+            {
+                return Json(new Models.ResponseViewModel
+                {
+                    IsSuccessful = false,
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
     }
 }
