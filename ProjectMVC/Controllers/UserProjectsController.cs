18c77ef5846ca93cffa5eada6fb57c0376b4e499
David Santafe <santafedavid01@gmail.com>
2019-04-13 09:25:22 -0500

    Agregar miembros al proyecto

--- ProjectMVC/Controllers/UserProjectsController.cs
+++ ProjectMVC/Controllers/UserProjectsController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using IdentitySample.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,29 +11,81 @@ namespace ProjectMVC.Controllers
 {
     public class UserProjectsController : Controller
     {
-        // GET: UserProjects
-        public ActionResult Index()
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
         {
-            var listUsers = new List<Logica.Models.ViewModels.UserProjectsIndexViewModel> {
-                new Logica.Models.ViewModels.UserProjectsIndexViewModel{
-                    Email = "dsantafe@navisaf.com",
-                    UserName = "dsantafe@navisaf.com",
-                    Id = "123"
-                }
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public UserProjectsController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserProjectsController()
+        {
+
+        }
+
+        // GET: UserProjects
+        public async Task<ActionResult> Index(int? projectId)
+        {
+            ApplicationUser user = await UserManager.FindByNameAsync(User.Identity.Name);
+
+            Logica.BL.Tenants tenants = new Logica.BL.Tenants();
+            var tenant = tenants.GetTenants(user.Id).FirstOrDefault();
+
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetProjects(projectId, null, null).FirstOrDefault();
+
+            Logica.BL.AspNetUsers aspNetUsers = new Logica.BL.AspNetUsers();
+            var listUsers = aspNetUsers.GetUsers(tenant.Id);
+
+            Logica.BL.UserProjects userProjects = new Logica.BL.UserProjects();
+            var listUsersProject = userProjects.GetUsersProject(projectId);
+
+            var listUsersResult = (from _users in listUsers
+                                   where !listUsersProject.Select(x => x.UserId).Contains(_users.Id)
+                                   select new Logica.Models.ViewModels.Users
+                                   {
+                                       Id = _users.Id,
+                                       Email = _users.Email,
+                                       UserName = _users.UserName
+                                   }).ToList();
+
+            var listUsersProjectResult = listUsersProject.Select(x => new Logica.Models.ViewModels.Users
+            {
+                Id = x.UserId,
+                Email = x.AspNetUsers.Email,
+                UserName = x.AspNetUsers.UserName
+            }).ToList();
+
+            var listUserProjectsIndexViewModel = new Logica.Models.ViewModels.UserProjectsIndexViewModel
+            {
+                Users = listUsersResult,
+                UsersProject = listUsersProjectResult
             };
 
-            var listUsersProject = new List<Logica.Models.ViewModels.UserProjectsIndexViewModel> {
-                new Logica.Models.ViewModels.UserProjectsIndexViewModel{
-                    Email = "lvaldes@navisaf.com",
-                    UserName = "lvaldes@navisaf.com",
-                    Id = "123"
-                }
-            };
+            ViewBag.Project = project;
 
-            ViewBag.ListUsers = listUsers;
-            ViewBag.ListUsersProject = listUsersProject;
+            return View(listUserProjectsIndexViewModel);
+        }
 
-            return View();
+        public ActionResult AddUser(int? projectId, string userId)
+        {
+            return RedirectToAction("Index", new { projectId });
+        }
+
+        public ActionResult RemoveUser(int? projectId, string userId)
+        {
+            return RedirectToAction("Index", new { projectId });
         }
     }
 }
