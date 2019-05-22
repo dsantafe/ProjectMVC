8b568f7d958dc9ebf65b1c7b2925c3fde4c22f78
David Santafe <santafedavid01@gmail.com>
2019-05-21 22:01:27 -0500

    Se agrego FullCalendar para visualización de las tareas, con metodo Get Ajax de jQuery

--- ProjectMVC/Controllers/TasksController.cs
+++ ProjectMVC/Controllers/TasksController.cs
@@ -87,5 +87,50 @@ namespace ProjectMVC.Controllers
 
             return View(model);
         }
+
+        public ActionResult Calendar(int? projectId)
+        {
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetProjects(projectId, null).FirstOrDefault();
+
+            ViewBag.Project = project;
+
+            return View();
+        }
+
+        public ActionResult GetTasksCalendar(int? projectId)
+        {
+            try
+            {
+                Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+                var listTasks = tasks.GetTasks(projectId, null);
+
+                var listTasksCalendarViewModel = listTasks.Select(x => new Logica.Models.ViewModels.TasksGetTasksCalendarViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Start = x.ExpirationDate.Value.AddDays(Convert.ToDouble(-x.RemainingWork)).ToString("yyyy-MM-dd HH:mm:ss"),
+                    End = x.ExpirationDate.Value.ToString("yyyy-MM-dd HH:mm:ss"),
+                    AllDay = false,
+                    Color = "#0027FF",
+                    TextColor = "#000000"
+                }).ToList();
+
+                return Json(new
+                {
+                    Data = listTasksCalendarViewModel,
+                    IsSuccessful = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new Logica.Models.ViewModels.ResponseViewModel
+                {
+                    IsSuccessful = false,
+                    Errors = new List<string> { ex.Message }
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
