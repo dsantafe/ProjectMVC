ae958ce11c2bb51813584d9d91cec538f995bb99
David Santafe <santafedavid01@gmail.com>
2019-08-25 15:39:27 -0500

    Create Activities

--- ProjectMVC.WS/Services/Test.asmx.cs
+++ ProjectMVC.WS/Services/Test.asmx.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Services;
+﻿using System.Web.Services;
 
 namespace ProjectMVC.WS.Services
 {
@@ -16,22 +12,14 @@ namespace ProjectMVC.WS.Services
     // [System.Web.Script.Services.ScriptService]
     public class Test : System.Web.Services.WebService
     {
+        /// <summary>
+        /// METODO QUE CREA LA ACTIVIDAD
+        /// </summary>
+        /// <param name="name">NOMBRE DE LA ACTIVIDAD</param>
         [WebMethod]
-        public string GetAgeInDays(int agesInDays)
+        public void CreateActivities(string name)
         {
-            return "Años - Meses - Dias";
-        }
-
-        [WebMethod]
-        public int GetAge(int yearOfBirthDay)
-        {
-            return DateTime.Now.Year - yearOfBirthDay;
-        }
-
-        [WebMethod]
-        public string GetName()
-        {
-            return "David Santafe";
+            
         }
     }
 }
