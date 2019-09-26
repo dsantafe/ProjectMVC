45699ad4a9c5e327d8d05e6b28abb20b7ac166a1
David Santafe <santafedavid01@gmail.com>
2019-09-25 23:03:28 -0500

    API Metodos GET y POST

---
+++ ProjectMVC.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Http;
+
+namespace ProjectMVC.API.Controllers
+{
+    [RoutePrefix("api/Activities")]
+    public class ActivitiesController : ApiController
+    {
+        [Route("GetActivities")]
+        public IHttpActionResult GetActivities()
+        {
+            try
+            {
+                Logica.Services.Activities activities = new Logica.Services.Activities();
+                var listActivities = activities.GetActivities();
+
+                return Ok(listActivities);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateActivity")]
+        public IHttpActionResult CreateActivity(Logica.Models.BindingModels.ActivitiesCreateBindingModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest();
+
+                Logica.Services.Activities activities = new Logica.Services.Activities();
+                activities.CreateActivity(model.Name);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
